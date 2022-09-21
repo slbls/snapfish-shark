9a34d22184791ce47e0a40f5b3efff0437db9b0f
Spencer Berenson <spencer@berenson.dev>
2022-09-21 14:49:54 -0400

    test: update startsWithVowel test for case

--- src/utils/startsWithVowel.test.ts
+++ src/utils/startsWithVowel.test.ts
@@ -1,12 +1,22 @@
 import { expect, it } from "vitest";
 import { startsWithVowel } from "./startsWithVowel";
 
-it("returns true when a given value starts with a vowel", () => {
+it("returns true when a given value starts with a lowercase vowel", () => {
 	const vowelWord = "alphabet";
 	expect(startsWithVowel(vowelWord)).toBe(true);
 });
 
-it("returns false when a given value does not start with a vowel", () => {
+it("returns true when a given value starts with an uppercase vowel", () => {
+	const vowelWord = "Alphabet";
+	expect(startsWithVowel(vowelWord)).toBe(true);
+});
+
+it("returns false when a given value starts with a lowercase consonant", () => {
 	const consonantWord = "letter";
+	expect(startsWithVowel(consonantWord)).toBe(false);
+});
+
+it("returns false when a given value starts with an uppercase consonant", () => {
+	const consonantWord = "Letter";
 	expect(startsWithVowel(consonantWord)).toBe(false);
 });
