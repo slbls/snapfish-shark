6eda5f01c2e98f5b43aa66c49af161335c859544
Spencer Berenson <spencer@berenson.dev>
2022-10-07 19:16:24 -0400

    fix: polyfill fetch for Vitest

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,13 +1,19 @@
 import { server } from "@/__mocks__/server";
 import matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
+import { fetch } from "cross-fetch";
 import { afterAll, afterEach, beforeAll, expect } from "vitest";
 
 expect.extend(matchers);
 
+// Fetch is not available in the LTS version of Node, so this polyfills it,
+// allowing for requests to be intercepted and mocked by MSW.
+globalThis.fetch = fetch;
+
 beforeAll(() => server.listen());
 
 afterEach(() => {
+	server.resetHandlers();
 	cleanup();
 });
 
