0e6e0b8b052809cd436601cf322a335ff8f504eb
Spencer Berenson <spencer@berenson.dev>
2022-10-07 18:40:18 -0400

    fix: remove URL search params from Snapfish config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,6 @@ export const SET_COOKIE_HEADER_NAME = "set-cookie";
 export const COOKIE_SEPARATOR_CHARACTER = ";";
 export const COOKIE_KEY_VALUE_SEPARATOR_CHARACTER = "=";
 
-export const SNAPFISH_LOGIN_URL =
-	"https://www.snapfish.com/loginto?=submit=true&componentID=1395868004571";
+export const SNAPFISH_LOGIN_URL = "https://www.snapfish.com/loginto";
 export const SNAPFISH_HOME_URL = "https://www.snapfish.com/home";
 export const SNAPFISH_OAUTH_COOKIE_NAME = "oa2";
