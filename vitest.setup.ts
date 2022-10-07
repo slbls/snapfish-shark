3e16ca7a3a452b2b18b8b9e235e3bf0987029219
Spencer Berenson <spencer@berenson.dev>
2022-10-07 17:55:03 -0400

    test: configure MSW server in Vitest setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,16 @@
 import matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
-import { afterEach, expect } from "vitest";
+import { setupServer } from "msw/node";
+import { afterAll, afterEach, beforeAll, expect } from "vitest";
 
 expect.extend(matchers);
+
+const server = setupServer();
+
+beforeAll(() => server.listen());
 
 afterEach(() => {
 	cleanup();
 });
+
+afterAll(() => server.close());
