29abc126d2bf6639eb598c9966e9f5dac866fa46
Spencer Berenson <spencer@berenson.dev>
2022-10-07 18:03:21 -0400

    feat: add Snapfish-related config values

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,13 @@
 export const APP_NAME = "Snapfish Shark";
 export const APP_DESCRIPTION = "🦈 Bare-bones Snapfish photo downloader.";
 export const HTML_TITLE_SEPARATOR = " | ";
+
+export const SET_COOKIE_HEADER_NAME = "set-cookie";
+export const COOKIE_SEPARATOR_CHARACTER = ";";
+export const COOKIE_KEY_VALUE_SEPARATOR_CHARACTER = "=";
+
+export const SNAPFISH_LOGIN_URL =
+	"https://www.snapfish.com/loginto?=submit=true&componentID=1395868004571";
+export const SNAPFISH_HOME_URL = "https://www.snapfish.com/home";
+export const SNAPFISH_IW_PRE_ACTIONS = "submit";
+export const SNAPFISH_OAUTH_COOKIE_NAME = "oa2";
